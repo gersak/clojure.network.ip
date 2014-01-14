59de090571ae47defaeb754e075d641a8dd47d5f
MasterBlaster <rgersak@virsrvmgmt15.ip.t-com.hr>
2014-01-14 17:53:41 +0100

    Starting with different approach

--- src/clojure/network/l3/ip.clj
+++ src/clojure/network/l3/ip.clj
@@ -2,10 +2,11 @@
   (:import [java.net InetAddress Inet4Address Inet6Address]
            [org.apache.commons.net.util SubnetUtils]
            [java.math BigInteger])
-  (:require [clojure.core.reducers :as r]))
+  (:require [clojure.core.reducers :as r])
+  (:import [clojure.lang Seqable SeqIterator ISeq]))
 
 
-(defprotocol IpAddress
+(defprotocol IpAddressfy
   (ip-address [address])
   (hostname [address]))
 
@@ -14,6 +15,14 @@
 
 (defn- make-ip-address [#^bytes ip]
   (InetAddress/getByAddress ip))
+
+(deftype IPv4Address [#^bytes address]
+  Object
+  (toString [_] (-> address make-ip-address (.getHostAddress))))
+
+(deftype IPv6Address [#^bytes address]
+  Object
+  (toString [_] (-> address make-ip-address (.getHostAddress))))
 
 (defn- get-mark [#^bytes ip-address ^Integer subnet]
   (if (<= (count ip-address) 4) (- 32 subnet) (- 128 subnet)))
@@ -35,7 +44,7 @@
 
 
 
-(defn subnet-count [x]
+(defn- subnet-count [x]
   "Returns set bits in byte"
   (let [testers (range 7 -1 -1)
         test! (map #(bit-test x %) testers)
@@ -44,11 +53,6 @@
     (if (not= subnet-bits all-bits) 
       (throw (Exception. (str "Subnet part: " x " is not valid subnet!")))
       subnet-bits)))
-
-
-
-
-
 
 (defn- #^String get-all-addresses-helper [#^bytes ip-address ^Integer subnet]
   (let [high-address (biginteger (get-max-address ip-address subnet))
@@ -60,14 +64,11 @@
                            .getHostAddress))]
     (map mapper (range low-address (inc high-address)))))
 
-
-
 (defn- is-in-network?-helper [#^bytes ip-address ^Integer subnet ^String q-address]
   (let [q-address (BigInteger. (.getAddress (InetAddress/getByName q-address)))
         h-address (BigInteger. (get-max-address ip-address subnet))
         l-address (BigInteger. (get-network-address ip-address subnet))]
     (boolean (and (>= h-address q-address) (<= l-address q-address)))))
-
 
 (defprotocol NetworkCommon
   (get-subnet [this] "Returns subnet in String form")
@@ -94,34 +95,35 @@
   (get-all-addresses [this] (get-all-addresses this true)))
 
 (extend java.net.InetAddress
-  IpAddress
+  IpAddressfy
   {:ip-address #(.getHostAddress %)
    :hostname #(.getHostName %)})
 
 (extend java.net.Inet4Address
-  IpAddress
+  IpAddressfy
   {:ip-address #(.getHostAddress %)
    :hostname #(.getHostName %)})
 
 (extend java.net.Inet6Address
-  IpAddress
+  IpAddressfy
   {:ip-address #(.getHostAddress %)
    :hostname #(.getHostName %)})
 
 (extend Network 
-  IpAddress
+  IpAddressfy
   {:ip-address #(-> % :ip-address .getHostAddress)
    :hostname #(-> % :ip-address .getHostName)})
 
 (extend java.lang.String
-  IpAddress
+  IpAddressfy
   {:ip-address #(when-let [[ip sub]  (clojure.string/split % #"/")]
-                  (.getHostAddress (java.net.InetAddress/getByName ip)))
+                    (.getHostAddress (java.net.InetAddress/getByName ip)))
    :hostname #(when-let [address (java.net.InetAddress/getByName %)]
                 (.getHostName address))}
   Networkfy
   {:network (fn 
-              ([x] (let [[ip sub] (clojure.string/split x #"/")
+              ([x] (let [x (re-find #"(?<=\s*)[^\s]*(?=\s*)" x)
+                         [ip sub] (clojure.string/split x #"/")
                          sub (if-not (string? sub) sub (Integer/parseInt sub))
                          ip (java.net.InetAddress/getByName ip)] 
                      (if (instance? Inet4Address ip) 
@@ -130,43 +132,13 @@
                        (do (assert (and (<= sub 128) (pos? sub)) "Subnet mask IPv6 is limited to 0-128 bits.")
                            (Network. ip sub)))))
               ([^String ip-address subnet]
-               (if (string? subnet)
-                 (do
-                   (assert (seq (re-find #"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" subnet)) (str "Subnet: " subnet " is not valid!"))
-                   (let [subnet (if (string? subnet)
-                                  (reduce + (map subnet-count (map read-string (clojure.string/split subnet #"\."))))
-                                  subnet)]
-                     (network (str ip-address "/" subnet))))
-                 (do
-                   (assert (and (<= subnet 32) (> subnet 0)) (str "Subnet: " subnet " is not valid!"))
-                   (network (str ip-address "/" subnet))))))})
-  ;;NetworkCommon
-  ;;{:get-subnet (fn [x] (-> x network get-subnet))}
-  ;;{:get-highest-address(fn [x] (-> x network get-highest-address))}
-  ;;{:get-lowest-address (fn [x] (-> x network get-lowest-address))}
-  ;;{:get-network (fn [x] (-> x network get-network))}
-  ;;{:is-in-network? (fn [x q-address] (->> x network (is-in-network? q-address)))}
-  ;;{:get-all-addresses (fn [x] (get-all-addresses (network x)))}
-  ;;{:get-all-addresses (fn [x inclusive?] (get-all-addresses (network x) inclusive?))})
-
-
-;;(def test-network "192.168.0.253/12")
-;;
-;;(def x (network test-network))
-;;
-;;(defn apache-test []
-;;  (let [addresses (time (.. (SubnetUtils. test-network) (getInfo) (getAllAddresses)))]
-;;    (println "Address count:" (count addresses))))
-;;
-;;(defn my-test []
-;;  (let [addresses (get-all-addresses (network test-network) false)]
-;;    (println (time (count addresses)))
-;;    (println (first addresses) (last addresses))
-;;    (println "Address count:" (count addresses))))
-;;
-;;
-;;(defn compare-speed []
-;;  (println "APACHE:")
-;;  (apache-test)
-;;  (println "MY:")
-;;  (my-test))
+               (cond 
+                 (string? subnet) (do
+                                    (assert (seq (re-find #"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" subnet)) (str "Subnet: " subnet " is not valid!"))
+                                    (let [subnet (if (string? subnet)
+                                                   (reduce + (map subnet-count (map read-string (clojure.string/split subnet #"\."))))
+                                                   subnet)]
+                                      (network (str ip-address "/" subnet))))
+                 (number? subnet) (do
+                                    (assert (and (<= subnet 32) (> subnet 0)) (str "Subnet: " subnet " is not valid!"))
+                                    (network (str ip-address "/" subnet))))))})
