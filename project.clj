b7834dfefd5b1152d9d7f476908c7f3c0843c830
MasterBlaster <rgersak@virsrvmgmt15.ip.t-com.hr>
2014-09-24 15:12:43 +0200

    This is first working version of clojure network library for easier network, and ip address computations

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject clojure.network "0.1.0-rc1"
+(defproject clojure.network "0.1.0-beta3"
   :description "FIXME: write description"
+  :aot [clojure.network.l3.ip.Network
+        clojure.network.l3.ip.IPAddress]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  ;;[jpcap/jpcap "1.0"]
                  [commons-net/commons-net "3.0.1"]])
--- src/clojure/network/l3/ip.clj
+++ src/clojure/network/l3/ip.clj
@@ -1,159 +1,68 @@
 (ns clojure.network.l3.ip
-  (:import [java.net InetAddress Inet4Address Inet6Address]
-           [org.apache.commons.net.util SubnetUtils]
-           [java.math BigInteger])
-  (:require [clojure.core.reducers :as r]))
+  (:refer-clojure :exclude [+ - inc dec])
+  (:require [clojure.network.l3.ip.IPAddress])
+  (:import [clojure.network.l3.ip IPAddress Network]
+           [java.lang UnsupportedOperationException]))
+
+(defn- num2ip [#^Number x]
+  (IPAddress. (.toByteArray (biginteger x))))
+
+(defn + 
+  ([] nil)
+  ([x] x)
+  ([x y] (num2ip (.add (biginteger x) (biginteger y))))
+  ([x y & more]
+   (reduce #(.add (biginteger %1) (biginteger %2)) (.add (biginteger x) (biginteger y)) more)))
+
+(defn inc
+  [x]
+  (num2ip (.add x BigInteger/ONE)))
+
+(defn dec 
+  [x]
+  (num2ip (.subtract x BigInteger/ONE)))
+
+(defn -
+  ([] nil)
+  ([x] x)
+  ([x y] (num2ip (.subtract (biginteger x) (biginteger y))))
+  ([x y & more]
+   (num2ip (reduce #(.subtract (biginteger %1) (biginteger %2)) (.subtract (biginteger x) (biginteger y)) more))))
+
+(defmethod print-method clojure.network.l3.ip.Network [n ^java.io.Writer w]
+  (.write w (apply str "Network: " (str n))))
+
+(defmethod print-method clojure.network.l3.ip.IPAddress [ip ^java.io.Writer w]
+  (.write w (str "IPAddress: " ip)))
+
+(. clojure.pprint/simple-dispatch addMethod clojure.network.l3.ip.Network #(.write ^java.io.Writer *out* (pr-str %)))
+(. clojure.pprint/simple-dispatch addMethod clojure.network.l3.ip.IPAddress #(.write ^java.io.Writer *out* (pr-str %)))
 
 
-(defprotocol IpAddress
-  (ip-address [address])
-  (hostname [address]))
-
-(defprotocol Networkfy
-  (networkfy [cidr] [ip-address subnet]))
-
-(defn- make-ip-address [#^bytes ip]
-  (InetAddress/getByAddress ip))
-
-(defn- get-mark [#^bytes ip-address ^Integer subnet]
-  (if (<= (count ip-address) 4) (- 32 subnet) (- 128 subnet)))
-
-(defn- get-network-address [#^bytes ip-address ^Integer subnet]
-  "Function takes ip address and subnet and returns network
-  IP address in byte-array form"
-  (let [mark (get-mark ip-address subnet)
-        ip (BigInteger. ip-address)]
-    (-> (.shiftRight ip mark) (.shiftLeft mark) (.toByteArray))))
-
-(defn- get-max-address  [#^bytes ip-address ^Integer subnet]
-  "Returns maximum ip address for given ip-address and subnet.
-  In other words, maximum address of network"
-  (let [mark (get-mark ip-address subnet)
-        ip (BigInteger. ip-address)
-        mask (BigInteger/valueOf (dec (bit-shift-left 1 mark)))]
-    (.toByteArray (.or ip mask))))
-
-(defn subnet-count [x]
-  "Returns set bits in byte"
-  (let [testers (range 7 -1 -1)
-        test! (map #(bit-test x %) testers)
-        subnet-bits (count (take-while true? test!))
-        all-bits (count (filter true? test!))]
-    (if (not= subnet-bits all-bits) 
-      (throw (Exception. (str "Subnet part: " x " is not valid subnet!")))
-      subnet-bits)))
-
-(defn- #^String get-all-addresses-helper [#^bytes ip-address ^Integer subnet]
-  (let [high-address (biginteger (get-max-address ip-address subnet))
-        low-address (biginteger (get-network-address ip-address subnet))
-        mapper (fn [x] (-> x 
-                           biginteger
-                           (.toByteArray)
-                           InetAddress/getByAddress
-                           .getHostAddress))]
-    (map mapper (range low-address (inc high-address)))))
+(defn make-ip-address 
+  "Returns IPAddress class instance for input data.
+  Data can be of String type, i.e. \"192.168.0.1\" or
+  it can be byte-array of size 4 or 16."[data]
+  (IPAddress. data))
 
 
+(defn make-network
+  "Returns Network class instance for input data. Function
+  accepts one or two argutmets. 
 
-(defn- is-in-network?-helper [#^bytes ip-address ^Integer subnet ^String q-address]
-  (let [q-address (BigInteger. (.getAddress (InetAddress/getByName q-address)))
-        h-address (BigInteger. (get-max-address ip-address subnet))
-        l-address (BigInteger. (get-network-address ip-address subnet))]
-    (boolean (and (>= h-address q-address) (<= l-address q-address)))))
-
-
-(defprotocol NetworkCommon
-  (get-highest-address [this] "Returns highest address in String form")
-  (get-lowest-address [this] "Returns lowest address in String from")
-  (get-network [this] "Returns CIDR notated String form")
-  (is-in-network? [this ^String ip-address] "Evaluates if ip-address is in this network")
-  (get-all-addresses [this] [this inclusive?] "Returns seq of addresses in this network. If second argument
-                                              is true than ALL adresses are returned. If false, than addresses
-                                              without first and last ip addresses are returned"))
-
-(defrecord Network [^InetAddress ip-address ^Integer subnet]
-  NetworkCommon
-  (get-highest-address [_] (-> (get-max-address (.getAddress ip-address) subnet) make-ip-address (.getHostAddress)))
-  (get-lowest-address [this] (-> (get-network-address (.getAddress (:ip-address this)) (:subnet this)) make-ip-address (.getHostAddress)))
-  (get-network [_] (-> (get-network-address (.getAddress ip-address) subnet) make-ip-address (.getHostAddress) (str "/" subnet)))
-  (is-in-network? [_ q-address] (is-in-network?-helper (.getAddress ip-address) subnet q-address))
-  (get-all-addresses [this inclusive?]
-    (let [addresses (get-all-addresses-helper (.getAddress ip-address) subnet)]
-      (if inclusive? 
-        addresses
-        (rest (butlast addresses)))))
-  (get-all-addresses [this] (get-all-addresses this true))
-  Object
-  (toString [this] (str (apply str (rest (str ip-address))) "/" subnet)))
-
-(extend java.net.InetAddress
-  IpAddress
-  {:ip-address #(.getHostAddress %)
-   :hostname #(.getHostName %)})
-
-(extend java.net.Inet4Address
-  IpAddress
-  {:ip-address #(.getHostAddress %)
-   :hostname #(.getHostName %)})
-
-(extend java.net.Inet6Address
-  IpAddress
-  {:ip-address #(.getHostAddress %)
-   :hostname #(.getHostName %)})
-
-(extend Network 
-  IpAddress
-  {:ip-address #(-> % :ip-address .getHostAddress)
-   :hostname #(-> % :ip-address .getHostName)})
-
-(extend java.lang.String
-  IpAddress
-  {:ip-address #(when-let [[ip sub]  (clojure.string/split % #"/")]
-                  (.getHostAddress (java.net.InetAddress/getByName ip)))
-   :hostname #(when-let [address (java.net.InetAddress/getByName %)]
-                (.getHostName address))}
-  Networkfy
-  {:networkfy (fn 
-                ([x] (let [[ip sub] (clojure.string/split x #"/")
-                           sub (if-not (string? sub) sub (Integer/parseInt sub))
-                           ip (java.net.InetAddress/getByName ip)] 
-                       (if (instance? Inet4Address ip) 
-                         (do (assert (and (<= sub 32) (pos? sub)) "Subnet mask IPv4 is limited to 0-32 bits.")
-                             (Network. ip sub))
-                         (do (assert (and (<= sub 128) (pos? sub)) "Subnet mask IPv6 is limited to 0-128 bits.")
-                             (Network. ip sub)))))
-                ([^String ip-address subnet]
-                 (if (string? subnet)
-                   (do
-                     (assert (seq (re-find #"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" subnet)) (str "Subnet: " subnet " is not valid!"))
-                     (let [subnet (if (string? subnet)
-                                    (reduce + (map subnet-count (map read-string (clojure.string/split subnet #"\."))))
-                                    subnet)]
-                       (networkfy (str ip-address "/" subnet))))
-                   (do
-                     (assert (and (<= subnet 32) (> subnet 0)) (str "Subnet: " subnet " is not valid!"))
-                     (networkfy (str ip-address "/" subnet))))))})
-
-(def test-network "192.168.0.253/12")
-
-(def ipv6-network "FE80:0000:0000:0000:0202:B3FF:FE1E:8329/106")
-
-(def x (networkfy ipv6-network))
-
-
-;;(defn apache-test []
-;;  (let [addresses (time (.. (SubnetUtils. test-network) (getInfo) (getAllAddresses)))]
-;;    (println "Address count:" (count addresses))))
-;;
-;;(defn my-test []
-;;  (let [addresses (get-all-addresses (network test-network) false)]
-;;    (println (time (count addresses)))
-;;    (println (first addresses) (last addresses))
-;;    (println "Address count:" (count addresses))))
-;;
-;;
-;;(defn compare-speed []
-;;  (println "APACHE:")
-;;  (apache-test)
-;;  (println "MY:")
-;;  (my-test))
+  In case of one argument, argument has to be cannoncial String
+  representation of network:
+  
+  v4 - \"192.168.0.23/24\"
+  v6 - \"2001:db8:85a3:0:0:8a2e:370:7334/120\"
+  
+  If there are 2 arguments as input, then first argument is representation of
+  IPAddress or string representation of IPAddress or byte[], and second argument can be
+  of type String or Integer
+  
+  Integer is used for CIDR notation and String type can be used for v4 subnet
+  masks i.e. 255.255.255.0"
+  ([^String network] (Network. network))
+  ([ip mask] (if (instance? IPAddress ip)
+               (Network. (str ip) mask)
+               (Network. ip mask))))
