9360c7357ebe2a127bc105a4a32aabf5d7e85dbd
Enzzo <souenzzo@gmail.com>
2024-08-21 13:31:16 -0300

    Bump clojure to make it work

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :source-paths ["src"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]]
+  :dependencies [[org.clojure/clojure "1.11.4"]
+                 [org.clojure/clojurescript "1.11.132"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :cljsbuild {:builds [{:id "network"
